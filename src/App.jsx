75f62df9e8799f3cfee559c5fa4f68a5da75bceb
Suriyaprakash-R-P <suriyaprakashrp2006@gmail.com>
2026-06-04 08:05:11 +0530

    Day 3 - Interactive map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function App() {
   return (
     <div>
       <h1>Groundwater Monitoring System</h1>
-      <h2>Day 2 Completed ✅</h2>
+
+      <MapContainer
+        center={[11.341, 77.717]} // Erode
+        zoom={10}
+        style={{ height: "500px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[11.341, 77.717]}>
+          <Popup>Erode Groundwater Monitoring Point</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
